a6e5735a2a1e79986009dba74c6638a801a63f4d
MichaelC045 <michael.christie@envisionly.tech>
2024-08-30 12:56:29 +0100

    Dialogue box styling

--- src/components/Newsletter.svelte
+++ src/components/Newsletter.svelte
@@ -16,24 +16,26 @@
         }
     }
 </script>
-
 <button on:click={openDialog}>Newsletter</button>
-<dialog bind:this={dialog}>
-    <div>
+<dialog bind:this={dialog} class ="w-3/12 h-9/12 border-2 border-black">
+    <div class="shadow-lg bg-white">
         <button on:click={closeDialog}>Close</button>
-    </div>
-    <form>
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" required>
+    <form class="">
+        <div class="flex w-full flex-col">
+            <p>Sign up for our Newsletter today!</p>
+            <label for="Email" class="text-left text-gray-700">Email</label>
+            <input
+                name="email"
+                type="email"
+                id="Email"
+                required
+                class=" rounded-md border-gray-200 text-sm focus:border-0 focus:ring-2 focus:ring-envisionlyGold"
+            />
+            <button
+            class="mt-4 rounded-full bg-envisionlyBlue px-4 py-2 text-white outline-none transition hover:outline-2 hover:outline-offset-4 hover:outline-envisionlyGold focus:outline-2 focus:outline-envisionlyGold disabled:cursor-not-allowed disabled:bg-envisionlyGold sm:mt-6">Accept</button>
+        </div>
     </form>
+</div>
 </dialog>
-<style>	dialog {
-    color: white;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 10%;
-    min-height: 100%;
-    background: var(--envisionlyBlue);
-    margin: 0;
-}</style>
+<style>
+</style>
