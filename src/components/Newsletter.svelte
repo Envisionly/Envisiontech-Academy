b7fb885a532237bb810c16b251210163fedd19a8
MichaelC045 <michael.christie@envisionly.tech>
2024-08-30 13:19:16 +0100

    Update Newsletter.svelte

--- src/components/Newsletter.svelte
+++ src/components/Newsletter.svelte
@@ -17,9 +17,10 @@
     }
 </script>
 <button on:click={openDialog}>Newsletter</button>
-<dialog bind:this={dialog} class ="w-3/12 h-9/12 border-2 border-black">
+<dialog bind:this={dialog} class ="w-1/3 h-9/12 border-2 border-black">
     <div class="shadow-lg bg-white">
-        <button on:click={closeDialog}>Close</button>
+        <button on:click={closeDialog}
+        class=" w-1/5 text-white bg-envisionlyBlue transition hover:outline-2 hover:outline-offset-4 hover:outline-envisionlyGold focus:outline-2 focus:outline-envisionlyGold">X</button>
     <form class="">
         <div class="flex w-full flex-col">
             <p>Sign up for our Newsletter today!</p>
@@ -34,6 +35,7 @@
             <button
             class="mt-4 rounded-full bg-envisionlyBlue px-4 py-2 text-white outline-none transition hover:outline-2 hover:outline-offset-4 hover:outline-envisionlyGold focus:outline-2 focus:outline-envisionlyGold disabled:cursor-not-allowed disabled:bg-envisionlyGold sm:mt-6">Accept</button>
         </div>
+        <br>
     </form>
 </div>
 </dialog>
