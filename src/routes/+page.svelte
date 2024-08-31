5c6f29ee6dd37a593162a8f44930e3cc63d3f4f0
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2024-08-31 21:10:18 +0100

    Added CTA component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import CTA from '$components/CTA.svelte';
+</script>
+
 <svelte:head>
 	<title>Envisiontech Academy: Master Tech Skills, Build Your Future</title>
 	<meta
@@ -7,65 +11,48 @@
 	<meta name="author" content="Paul Geoghegan" />
 </svelte:head>
 
-<div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-	<div class="mx-auto max-w-lg text-center">
-		<h1
-			class="mb-4 border-b border-envisionlyGold pb-4 text-3xl font-bold uppercase text-envisionlyBlue sm:text-4xl"
-		>
-			Master Tech Skills, Build Your Future.
-		</h1>
-		<p class="">
-			Welcome to your one-stop shop for mastering the skills that power the digital world! This
-			website is your launchpad into a universe of knowledge, encompassing everything from web
-			development and programming to design, data analysis, and more. Whether you're a complete
-			beginner or a seasoned professional seeking to expand your horizons, we offer a wealth of
-			resources and comprehensive courses to guide you on your journey. Dive in, explore, and unlock
-			your potential to create, analyze, and innovate in the ever-evolving digital landscape.
-		</p>
-	</div>
-	<div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+<CTA
+	imageSRC="/homepage.jpg"
+	headingText="Master Tech Skills. Build Your Future."
+	paragraphText="Welcome to your gateway to mastering the digital world! From web development to data analysis, our courses cater to beginners and pros alike. Explore, learn, and unlock your potential to create and innovate in today’s tech-driven landscape."
+/>
+
+<div class="px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+	<div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
 		<section class="block rounded-xl border border-gray-300 p-8 shadow-xl">
-			<h2 class="mb-2 border-b border-gray-500 pb-2 text-xl font-semibold uppercase sm:text-2xl">
+			<h2
+				class="mb-2 border-b border-envisionlyGold pb-2 text-xl font-semibold uppercase text-envisionlyBlue sm:text-2xl"
+			>
 				Why Us?
 			</h2>
 
 			<p class="text-sm">
-				We are passionate about empowering individuals to learn and grow in the digital age. Our
-				mission is to provide accessible, high-quality education to anyone who wants to develop
-				their technical skills and build a successful career in the tech industry. Our courses are
-				designed to be engaging, interactive, and practical, with a focus on real-world applications
-				and projects. We believe that learning should be fun, challenging, and rewarding, and we are
-				committed to helping you achieve your goals and realize your dreams.
+				We empower individuals to thrive in the digital age with accessible, high-quality tech
+				education. Our engaging courses focus on real-world applications, making learning fun,
+				practical, and rewarding. Let us help you build the skills for a successful tech career.
 			</p>
 		</section>
 		<section class="block rounded-xl border border-gray-300 p-8 shadow-xl">
 			<h2 class="mb-2 border-b border-gray-500 pb-2 text-xl font-semibold uppercase sm:text-2xl">
 				Our Courses
 			</h2>
-			<div class="my-2 h-[1px] w-[100%] bg-envisionlyGold"></div>
+
 			<p class="mt-1 text-sm">
-				Our courses cover a wide range of topics, including web development, programming, design,
-				data analysis, and more. Whether you're interested in learning the basics of HTML and CSS,
-				mastering the intricacies of JavaScript and React, or exploring the world of data science
-				and machine learning, we have something for everyone. Our courses are designed to be
-				flexible, self-paced, and accessible to learners of all levels, from beginners to advanced
-				professionals. We offer a variety of formats, including video tutorials, interactive
-				exercises, quizzes, and projects, to ensure that you get the most out of your learning
-				experience.
+				Our courses span web development, programming, design, data analysis, and more. From HTML
+				basics to advanced JavaScript, Svelte, and data science, we offer flexible, self-paced
+				learning for all levels. Enjoy video tutorials, interactive exercises, quizzes, and hands-on
+				projects to enhance your skills.
 			</p>
 		</section>
 		<section class="block rounded-xl border border-gray-300 p-8 shadow-xl">
 			<h2 class="mb-2 border-b border-gray-500 pb-2 text-xl font-semibold uppercase sm:text-2xl">
 				Get Started
 			</h2>
-			<div class="my-2 h-[1px] w-[100%] bg-envisionlyGold"></div>
-			<p class="mt-1 text-sm">
-				Ready to take the next step in your learning journey? Sign up for a free account today and
-				start exploring our courses. Whether you're looking to learn a new skill, advance your
-				career, or launch your own startup, we have the resources and support you need to succeed.
-				Join our community of learners, mentors, and experts, and discover the power of technology
-				to transform your life and the world around you. Envisiontech Academy: Master Tech Skills,
-				Build Your Future.
+
+			<p class="text-sm">
+				Ready to level up your skills? Sign up for a free account and explore our courses. Whether
+				learning a new skill, advancing your career, or starting a venture, we’ve got the resources
+				to help you succeed. Join our community and transform your future with Envisiontech Academy.
 			</p>
 		</section>
 	</div>
