b7a6c6b5c73c0e9911daf0393f3644c20d912863
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2024-08-31 13:01:25 +0100

    Did some styling on the newsletter dialog

--- src/components/Newsletter.svelte
+++ src/components/Newsletter.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-	let dialog: HTMLDialogElement;
+	let dialog: any;
 
 	function openDialog() {
 		if (dialog) {
-			dialog.showModal();
+			dialog.open = true;
 		} else {
 			console.error('Dialog element is not defined');
 		}
 	}
+
 	function closeDialog() {
 		if (dialog) {
-			dialog.close();
+			dialog.open = false;
 		} else {
 			console.error('Dialog element is not defined');
 		}
@@ -22,41 +23,58 @@
 	}
 </script>
 
-<button onclick={openDialog} class="text-center text-sm text-gray-700"
+<button
+	onclick={openDialog}
+	class="text-sm text-envisionlyLightBlue hover:text-envisionlyGold focus:text-envisionlyGold active:text-envisionlyTransparentGold"
 	>Sign up to our Newsletter</button
 >
 
-<dialog bind:this={dialog} class="w-2/5 h-9/12 border border-envisionlyGold">
-	<div class="shadow-lg bg-white">
+<wa-dialog style="--spacing:0;" lightDismiss bind:this={dialog}>
+	<div class="flex justify-end">
 		<button
 			onclick={closeDialog}
-			class=" w-8 h-1/3 text-white bg-envisionlyBlue outline-none transition hover:border-2 hover:border-black focus:border-2 focus:border-envisionlyGold disabled:cursor-not-allowed disabled:bg-envisionlyGold"
-			>X</button
+			class="rounded-bl-xl bg-envisionlyLightBlue px-3 py-1 text-white hover:bg-envisionlyGold focus:bg-envisionlyGold active:bg-envisionlyTransparentGold"
+			aria-label="Close"><span class="fa-light fa-times"></span></button
 		>
-		<form method="POST" class="flex flex-col items-center gap-6">
-			<div class="flex w-full flex-col">
-				<p>Enter your email below to register for our newsletter</p>
-				<label for="Email" class="text-left text-gray-700">Email</label>
-				<input
-					name="email"
-					type="email"
-					id="Email"
-					required
-					class=" w-3/4 rounded-md border-gray-200 text-sm focus:border-0 focus:ring-2 focus:ring-envisionlyGold"
-				/>
+	</div>
+	<form method="POST" class="flex flex-col gap-6 p-8">
+		<h2 class="my-2 text-center text-3xl font-semibold uppercase">Register for our newsletter</h2>
 
-				<input type="checkbox" id="terms" name="terms" required />
-				<label for="terms" class="text-left text-gray-700">
+		<div class="flex flex-col">
+			<label for="Email" class="text-left text-gray-700">Email</label>
+			<!-- svelte-ignore a11y_autofocus -->
+			<input
+				autofocus
+				name="email"
+				type="email"
+				id="Email"
+				required
+				class=" rounded-md border-gray-200 text-sm focus:border-0 focus:ring-2 focus:ring-envisionlyGold"
+			/>
+		</div>
+
+		<div class="flex gap-2">
+			<div>
+				<input
+					type="checkbox"
+					class="checked:bg-envisionlyLightBlue checked:hover:bg-envisionlyGold checked:hover:outline-envisionlyGold focus:outline-envisionlyLightBlue checked:focus:bg-envisionlyGold"
+					id="accept"
+					name="accept"
+					required
+				/>
+			</div>
+			<div>
+				<label for="accept" class="text-left text-sm text-gray-700">
 					By checking this box, you agree to our
 					<a href="/terms" class="text-envisionlyBlue">terms and conditions</a> and
 					<a href="/privacy" class="text-envisionlyBlue">privacy policy</a>
 				</label>
-
-				<button
-					class=" w-3/4 mt-4 rounded-full bg-envisionlyBlue px-4 py-2 text-white outline-none transition hover:outline-2 hover:outline-offset-4 hover:outline-envisionlyGold focus:outline-2 focus:outline-envisionlyGold disabled:cursor-not-allowed disabled:bg-envisionlyGold sm:mt-6"
-					>Sign Up</button
-				>
 			</div>
-		</form>
-	</div>
-</dialog>
+		</div>
+
+		<button
+			class="mx-auto w-fit rounded-xl bg-envisionlyLightBlue px-4 py-2 text-white hover:bg-envisionlyGold focus:bg-envisionlyGold active:bg-envisionlyTransparentGold"
+			>Sign Up</button
+		>
+	</form>
+</wa-dialog>
