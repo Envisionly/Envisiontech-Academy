7f334b4ee8d5e87797183e393d0ede54fef88129
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2024-09-06 09:18:20 +0100

    Made some small improvements to the drawer

--- src/components/TableOfContents.svelte
+++ src/components/TableOfContents.svelte
@@ -5,7 +5,7 @@
 	let sideBarDialog: HTMLDialogElement | undefined = $state(undefined);
 	let { category }: { category: categoryType } = $props();
 	let isMobile: boolean | undefined = $state(undefined);
-	let drawer: any;
+	let drawer: any = $state(undefined);
 
 	onMount(() => {
 		isMobile = window.innerWidth < 768;
@@ -16,7 +16,7 @@
 </script>
 
 {#if isMobile}
-	<dialog bind:this={sideBarDialog} class="top-0 left-0 min-h-full border-r border-gray-300">
+	<dialog bind:this={sideBarDialog} class="left-0 top-0 min-h-full border-r border-gray-300">
 		<div class="flex flex-col">
 			<button class="self-end" onclick={() => sideBarDialog?.close()}>Close</button>
 			{@render sideBarContents()}
@@ -25,47 +25,54 @@
 
 	<button onclick={() => sideBarDialog?.showModal()}>Show Sidebar</button>
 {:else}
-	<section class=" flex flex-col">
+	<!--Drawer for more course content-->
+	<wa-drawer label="Course Navigation" lightDismiss bind:this={drawer}>
+		<!-- svelte-ignore a11y_autofocus -->
+		<button
+			autofocus
+			data-drawer="close"
+			class=" rounded bg-envisionlyLightBlue px-2 py-2 font-bold text-white hover:bg-blue-500"
+			aria-label="Close">View Page</button
+		>
 		{@render sideBarContents()}
-	</section>
-	<div class="flex justify-end ">
-	<button class=" h-[15%] bg-envisionlyLightBlue hover:bg-blue-500 text-white font-bold rounded"
-	onclick={() => drawer.open = true}>More Courses</button>
-</div>
+	</wa-drawer>
+
+	<div class="flex justify-end">
+		<button
+			class=" h-[15%] rounded bg-envisionlyLightBlue font-bold text-white hover:bg-blue-500"
+			onclick={() => (drawer.open = true)}>Open Course Navigation</button
+		>
+	</div>
 {/if}
 
 {#snippet sideBarContents()}
-
-<!--Drawer for more course content-->
-<wa-drawer aria-label="Course"  bind:this={drawer}>
-	<button data-drawer="close" class=" bg-envisionlyLightBlue hover:bg-blue-500 text-white py-2 px-2 font-bold rounded" aria-label="Close">View Page</button>
 	<div class="flex flex-col">
 		<img
-			class="w-[80%] mx-auto object-contain"
+			class="mx-auto w-[80%] object-contain"
 			src={`/courseImages/${category.image}`}
 			alt={`${category.subCategory} logo`}
 		/>
 		<h2 class="invisible">{category.subCategory}</h2>
 	</div>
 
-	
-	<nav aria-label="Course content" class="flex flex-col whitespace-normal space-y-4">
+	<nav aria-label="Course content" class="flex flex-col space-y-4 whitespace-normal">
 		{#each category.lessons as section}
 			<!-- svelte-ignore attribute_quoted -->
 			<wa-details class="font-bold drop-shadow-md">
-				<h3 class="text-lg"
-				slot="summary">{section.section}</h3>
-			<ul class=" text-md font-normal ">
-				{#each section.lessons as lesson}
-					<li class="text-envisionlyLightBlue hover:text-envisionlyGold focus:text-envisionlyGold active:text-envisionlyTransparentGold">
-						<a href={`/learning/${category.subCategorySlug}/${section.sectionSlug}/${lesson.slug}`}
-							>{lesson.title}</a
+				<h3 class="text-lg" slot="summary">{section.section}</h3>
+				<ul class=" text-md font-normal">
+					{#each section.lessons as lesson}
+						<li
+							class="text-envisionlyLightBlue hover:text-envisionlyGold focus:text-envisionlyGold active:text-envisionlyTransparentGold"
 						>
-					</li>
-				{/each}
-			</ul>
-		</wa-details>
+							<a
+								href={`/learning/${category.subCategorySlug}/${section.sectionSlug}/${lesson.slug}`}
+								>{lesson.title}</a
+							>
+						</li>
+					{/each}
+				</ul>
+			</wa-details>
 		{/each}
 	</nav>
-</wa-drawer>
 {/snippet}
