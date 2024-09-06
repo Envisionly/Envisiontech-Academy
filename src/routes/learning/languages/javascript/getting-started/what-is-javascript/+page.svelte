2bfef0c31623f992935e91a6831a632267c957bc
MichaelC045 <michael.christie@envisionly.tech>
2024-09-06 12:41:59 +0100

    placing breadcrumb

--- src/routes/learning/languages/javascript/getting-started/what-is-javascript/+page.svelte
+++ src/routes/learning/languages/javascript/getting-started/what-is-javascript/+page.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+	import { javaScriptLessons } from '$utils/languages/javascript';
+	import TableOfContents from '$components/TableOfContents.svelte';
+	import Breadcrumb from '$components/Breadcrumb.svelte';
+</script>
+<Breadcrumb />
 <h1>Intro to JavaScript</h1>
