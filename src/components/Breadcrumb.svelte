a1b1e8c3da046c2aabc74bdf712727934410361a
MichaelC045 <michael.christie@envisionly.tech>
2024-09-06 11:55:58 +0100

    breadcrumb links to routes

--- src/components/Breadcrumb.svelte
+++ src/components/Breadcrumb.svelte
@@ -1,19 +1,25 @@
-<script lang="ts">
-    	import { onMount } from 'svelte';
-
-let pathSegments = [];
-
-onMount(() => {
-  const path = window.location.pathname;
-  pathSegments = path.split('/').filter(segment => segment);
-});
-
-</script>
-
-<wa-breadcrumb>
-	{#each pathSegments as segment, index}
-	  <wa-breadcrumb-item>
-		{segment}
-	  </wa-breadcrumb-item>
-	{/each}
+<script>
+    import { onMount } from 'svelte';
+  
+    let pathSegments = [];
+    let breadcrumbLinks = [];
+  
+    onMount(() => {
+      const path = window.location.pathname;
+      pathSegments = path.split('/').filter(segment => segment);
+  
+      // Construct breadcrumb links
+      breadcrumbLinks = pathSegments.map((segment, index) => {
+        const url = '/' + pathSegments.slice(0, index + 1).join('/');
+        return { segment, url };
+      });
+    });
+  </script>
+  
+  <wa-breadcrumb>
+    {#each breadcrumbLinks as { segment, url }, index}
+      <wa-breadcrumb-item>
+        <a href={url}>{segment}</a>
+      </wa-breadcrumb-item>
+    {/each}
   </wa-breadcrumb>
