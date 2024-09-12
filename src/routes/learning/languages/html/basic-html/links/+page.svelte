f33af141d11117a6a39c00e05caad925197a9ca7
MichaelC045 <michael.christie@envisionly.tech>
2024-09-12 16:15:56 +0100

    Update +page.svelte

---
+++ src/routes/learning/languages/html/basic-html/links/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import ContentBlock from '$components/ContentBlock.svelte';
+</script>
+
+<svelte:head>
+	<title>HTML Links</title>
+	<meta name="description" content="Learn how links work in HTML in this free course." />
+	<meta name="author" content="Paul Geoghegan" />
+</svelte:head>
+
+<h2 class="text-xl font-bold">HTML Links</h2>
+<p>
+    Links are a fundamental part of the web. They are what allow us to navigate from one page to another. In HTML, links are created using the <span class ="font-bold">&lt;a&gt; anchor </span> tag.
+</p><br>
