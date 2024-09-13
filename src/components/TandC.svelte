a6d1dabbb9bb93cecb993929e6e2599f689e0ecc
MichaelC045 <michael.christie@envisionly.tech>
2024-09-13 09:51:39 +0100

    Terms and conditions

---
+++ src/components/TandC.svelte
@@ -0,0 +1,30 @@
+<h1>Terms and Conditions</h1>
+<h2>LAST UPDATED: 13/09/2024</h2>
+<p>These terms and conditions apply to the website www.envisiontechacademy.com. The use of the words
+    "we", "us" and "our" refer to Envision Tech Academy. The use of the words "you" and "your" refer to
+    the user of the website. By using this website, you agree to the terms and conditions set out below.
+    If you do not agree to these terms and conditions, please do not use this website.
+</p>
+<ul>
+    <li>1. General</li>
+    <li>2. Privacy Policy</li>
+    <li>3. Cookies</li>
+    <li>4. User Accounts</li>
+    <li>5. User Content</li>
+    <li>6. Intellectual Property</li>
+    <li>7. Limitation of Liability</li>
+    <li>8. Governing Law</li>
+    <li>9. Contact Us</li>
+</ul>
+<h3>1. General</h3>
+<p>
+    1.1 We reserve the right to change these terms and conditions at any time. Any changes will take
+    effect immediately upon publication on the website. It is your responsibility to read the terms and
+    conditions regularly to ensure you are aware of any changes.
+</p>
+<p>
+    1.2 We reserve the right to change or remove any part of the website at any time without notice.
+</p>
+<p>
+    1.3 We reserve the right to refuse access to the website to any user at any time without notice.
+</p>
