f3ad37781b401b6c2ab34662d6ac2701108e479d
MichaelC045 <michael.christie@envisionly.tech>
2024-09-13 12:43:18 +0100

    moved privacy to own page

---
+++ src/routes/privacy/page.svelte
@@ -0,0 +1,12 @@
+<h1 class="text-2xl font-bold">Privacy Policy</h1>
+<p>
+    We take your privacy seriously. Please read our Privacy Policy to understand how we collect,
+    store and use your personal information.
+</p>
+<p>
+    By using this website, you consent to the collection, storage and use of your personal
+    information in accordance with our Privacy Policy.
+</p>
+<p>
+
+</p>
--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -1,93 +1,89 @@
 
-<h1>Terms and Conditions</h1>
-<h2>LAST UPDATED: 13/09/2024</h2>
+<h1 class="font-bold text-2xl">Terms and Conditions</h1>
+<h2 class="font-semi-bold text-sm">LAST UPDATED: 13/09/2024</h2>
+<br>
 <p>These terms and conditions apply to the website www.envisiontechacademy.com. The use of the words
-    "we", "us" and "our" refer to Envision Tech Academy. The use of the words "you" and "your" refer to
+    "we", "us" and "our" refer to Envisiontech Academy. The use of the words "you" and "your" refer to
     the user of the website. By using this website, you agree to the terms and conditions set out below.
     If you do not agree to these terms and conditions, please do not use this website.
 </p>
 <ul>
-    <li>1. General</li>
-    <li>2. Privacy Policy</li>
-    <li>3. Cookies</li>
-    <li>4. User Accounts</li>
-    <li>5. User Rights</li>
-    <li>6. Intellectual Property</li>
-    <li>7. Limitation of Liability</li>
-    <li>8. Governing Law</li>
-    <li>9. Contact Us</li>
+    <li>General</li>
+    <li>Cookies</li>
+    <li>User Accounts</li>
+    <li>User Rights</li>
+    <li>Intellectual Property</li>
+    <li>Limitation of Liability</li>
+    <li>Governing Law</li>
+    <li>Contact Us</li>
 </ul>
-<h3>1. General</h3>
+<br>
+<h3 class="text-xl font-bold">General</h3>
 <p>
-    1.1 We reserve the right to change these terms and conditions at any time. Any changes will take
+    We reserve the right to change these terms and conditions at any time. Any changes will take
     effect immediately upon publication on the website. It is your responsibility to read the terms and
     conditions regularly to ensure you are aware of any changes.
 </p>
 <p>
-    1.2 We reserve the right to change or remove any part of the website at any time without notice.
+    We reserve the right to change or remove any part of the website at any time without notice.
 </p>
 <p>
-    1.3 We reserve the right to refuse access to the website to any user at any time without notice.
+    We reserve the right to refuse access to the website to any user at any time without notice.
 </p>
 <p>
-    1.4 We reserve the right to suspend or terminate your account at any time without notice.
+    We reserve the right to suspend or terminate your account at any time without notice.
 </p>
 <p>
-    1.5 We reserve the right to take legal action against any user who breaches these terms and
+    We reserve the right to take legal action against any user who breaches these terms and
     conditions.
 </p>
 
-<h3>2. Privacy Policy</h3>
+<br>
+<h3 class="text-xl font-bold">Cookies</h3>
 <p>
-    2.1 We take your privacy seriously. Please read our Privacy Policy to understand how we collect,
-    store and use your personal information.
-</p>
-<p>
-    2.2 By using this website, you consent to the collection, storage and use of your personal
-    information in accordance with our Privacy Policy.
-</p>
-<p>
-
-</p>
-<h3>3. Cookies</h3>
-<p>
-    3.1 We use cookies on this website. By using this website, you consent to the use of cookies in
+    We use cookies on this website. By using this website, you consent to the use of cookies in
     accordance with our Cookies Policy.
 </p>
-<h3>4. User Accounts</h3>
+<br>
+<h3 class="text-xl font-bold">User Accounts</h3>
 <p>
-    4.1 You may be required to create a user account to access certain parts of the website. You are
+    You may be required to create a user account to access certain parts of the website. You are
     responsible for maintaining the confidentiality of your account details and password.
 </p>
 <p>
-    4.2 You must not share your account details or password with any other person.
+    You must not share your account details or password with any other person.
 </p>
-<h3>5. User Rights</h3>
+<br>
+<h3 class="text-xl font-bold">User Rights</h3>
 <p>
-    5.1 You reserve the right, within the first 14 days of purchase, to request a refund for any course.
+    You reserve the right, within the first 14 days of purchase, to request a refund for any course.
 </p>
-<h3>6. Intellectual Property</h3>
+<br>
+<h3 class="text-xl font-bold">Intellectual Property</h3>
 <p>
-    6.1 All intellectual property rights in the website and its content are owned by us or our licensors.
+    All intellectual property rights in the website and its content are owned by us or our licensors.
 </p>
 <p>
-    6.2 You may not copy, reproduce, distribute, or create derivative works from any part of the website
+    You may not copy, reproduce, distribute, or create derivative works from any part of the website
     without our prior written consent.
 </p>
-<h3>7. Limitation of Liability</h3>
+<br>
+<h3 class="text-xl font-bold">Limitation of Liability</h3>
 <p>
-    7.1 We will not be liable for any loss or damage arising from your use of the website or any content
+    We will not be liable for any loss or damage arising from your use of the website or any content
     posted on the website.
 </p>
 <p>
-    7.2 We will not be liable for any loss or damage arising from the use of any third-party websites
+    We will not be liable for any loss or damage arising from the use of any third-party websites
     linked to from the website.
 </p>
-<h3>8. Governing Law</h3>
+<br>
+<h3 class="text-xl font-bold">Governing Law</h3>
 <p>
-    8.1 These terms and conditions are governed by the laws of Republic of Ireland.
+    These terms and conditions are governed by the laws of Republic of Ireland.
 </p>
-<h3>9. Contact Us</h3>
+<br>
+<h3 class="text-xl font-bold">Contact Us</h3>
 <p>
-    9.1 If you have any questions about these terms and conditions, please contact us at
+    If you have any questions about these terms and conditions, please contact us at placeholder@envisionly.tech
 </p>
