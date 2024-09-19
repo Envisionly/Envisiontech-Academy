d0bcbe456ae851841a31fc63455962650581ce2f
MichaelC045 <michael.christie@envisionly.tech>
2024-09-19 16:59:36 +0100

    renamed file, updated policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,41 @@
+<svelte:head>
+  <title>Privacy Policy - Envisiontech Academy</title>
+  <meta name="description" content="Read the terms and conditions for using Envisiontech Academy's website and services." />
+</svelte:head>
+
+<h1 class="text-2xl font-bold">Privacy Policy</h1>
+<p>
+    We take your privacy seriously. Please read our Privacy Policy to understand how we collect,
+    store and use your personal information.
+</p>
+<p>
+    By using this website, you consent to the collection, storage and use of your personal
+    information in accordance with our Privacy Policy.
+</p>
+<br>
+<h3 id="cookies" class="mb-2 border-b border-envisionlyGold pb-2 font-semibold uppercase text-envisionlyBlue text-xl">
+    How we will use your data
+</h3>
+
+<ul>The personal data we collect, hold and retain from you is used to:
+    <li>- Register for our classes, and online courses</li>
+    <li>- Process your payments</li>
+    <li>- Send you information about our services</li>
+    <li>- Send you our newsletter 	&#40;optional&#41;</li>
+</ul>
+<br>
+<h3 id="cookies" class="mb-2 border-b border-envisionlyGold pb-2 font-semibold uppercase text-envisionlyBlue text-xl">
+    How we will protect your data
+</h3>
+<p>
+    We take the security of your data seriously. We have put in place appropriate security measures to
+    prevent your personal data from being accidentally lost, used or accessed in an unauthorised way,
+    altered or disclosed.
+</p>
+<p>
+    If you feel your data is not being handled correctly, or you are unhappy with our response to any
+    concerns/risks, please contact us.
+</p>
+<h3 id="cookies" class="mb-2 border-b border-envisionlyGold pb-2 font-semibold uppercase text-envisionlyBlue text-xl">
+    Data Protection Law Rights
+</h3>
