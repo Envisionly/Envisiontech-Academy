3953b56ab98ca02860ed1627bebbbd9a12455df8
MichaelC045 <michael.christie@envisionly.tech>
2024-09-19 14:54:47 +0100

    Privacy Policy
    Links to privacy policy and T+C's open in new tab now

--- src/routes/privacy/page.svelte
+++ src/routes/privacy/page.svelte
@@ -7,6 +7,10 @@
     By using this website, you consent to the collection, storage and use of your personal
     information in accordance with our Privacy Policy.
 </p>
+<h3 id="cookies" class="mb-2 border-b border-envisionlyGold pb-2 font-semibold uppercase text-envisionlyBlue text-xl">
+    How we will use your data
+</h3>
+
 <p>
 
 </p>
