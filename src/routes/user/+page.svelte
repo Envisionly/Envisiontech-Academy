f01bf6a40b5863a4d7442af4adafa05f9e2b98e8
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2024-11-18 12:35:14 +0000

    Moved sign-out to it's own api root

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -2,6 +2,14 @@
 	import { enhance } from '$app/forms';
 
 	const { data } = $props();
+	let dialog: HTMLDialogElement;
+
+	async function signOut() {
+		const response = await fetch('/api/user/sign-out', { method: 'get' });
+		if (response.ok) {
+			location.href = '/';
+		}
+	}
 </script>
 
 <svelte:head>
@@ -23,6 +31,21 @@
 	<p>Something went wrong while trying to load your profile!</p>
 {/if}
 
-<form method="post" use:enhance>
-	<button>Sign out</button>
-</form>
+<h2>Actions</h2>
+<button onclick={signOut}>Sign out</button>
+<button onclick={() => dialog.showModal}>Delete account</button>
+
+<dialog
+	bind:this={dialog}
+	aria-labelledby="Delete account"
+	aria-describedby="Are you sure you want to delete your account?"
+>
+	<h2 id="Delete account">Delete account</h2>
+	<p id="Are you sure you want to delete your account?">
+		Are you sure you want to delete your account?
+	</p>
+	<form method="post" action="/api/user/delete" use:enhance>
+		<button type="submit">Yes, delete my account</button>
+		<button type="button" onclick={() => dialog.close()}>No, keep my account</button>
+	</form>
+</dialog>
