93d77ac03b294da591d92b9a6a078db2d40a3dae
MichaelC045 <michael.christie@envisionly.tech>
2025-01-24 13:30:44 +0000

    login button styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,5 +6,14 @@
 	<title>Sign In</title>
 </svelte:head>
 
-<h1>Sign In</h1>
-<button on:click={() => goto('/login/github')}>Sign in with GitHub</button>
+<div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-8 w-full max-w-md rounded-lg shadow-lg flex flex-col gap-4">
+        <h1 class="text-2xl font-medium text-center">Sign In</h1>
+        <div class="flex justify-center gap-4">
+            <button class="bg-purple-800 text-white py-2 px-4 rounded flex items-center gap-2 hover:bg-purple-700" on:click={() => goto('/login/github')}>
+                Sign in with GitHub
+                <span class="fa-brands fa-github text-xl"></span>
+            </button>
+        </div>
+    </div>
+</div>
