a369260ab2d209f9e729c12a3a2cc22dffefec59
Paul Geoghegan <mrpaultdgeoghegan@gmail.com>
2025-02-12 09:37:05 +0000

    Added try catch for db statements

--- src/lib/server/learning.ts
+++ src/lib/server/learning.ts
@@ -2,75 +2,105 @@ import { prismaClient } from './connections';
 import type { sectionType, courseType, moduleType, lessonType } from '../../ambient';
 
 export async function getSections(): Promise<sectionType[]> {
-	return prismaClient.section.findMany();
+	try {
+		return prismaClient.section.findMany();
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
 
 export async function getCourses(): Promise<courseType[]> {
-	return prismaClient.course.findMany();
+	try {
+		return prismaClient.course.findMany();
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
 
 export async function getModules(): Promise<moduleType[]> {
-	return prismaClient.module.findMany();
+	try {
+		return prismaClient.module.findMany();
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
 
 export async function getLessons(): Promise<lessonType[]> {
-	return prismaClient.lesson.findMany();
+	try {
+		return prismaClient.lesson.findMany();
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
 
 export async function getSectionsWithCoursesModulesAndLessons(): Promise<sectionType[]> {
-	return prismaClient.section.findMany({
-		include: {
-			courses: {
-				include: {
-					modules: {
-						include: {
-							lessons: true
+	try {
+		return prismaClient.section.findMany({
+			include: {
+				courses: {
+					include: {
+						modules: {
+							include: {
+								lessons: true
+							}
 						}
 					}
 				}
 			}
-		}
-	});
+		});
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
 
 export async function getCourseModulesAndLessonsBySlug(
 	sectionSlug: string,
 	courseSlug: string
 ): Promise<sectionType | null> {
-	return await prismaClient.section.findFirst({
-		where: {
-			slug: sectionSlug
-		},
-		select: {
-			name: true,
-			slug: true,
-			courses: {
-				select: {
-					name: true,
-					slug: true,
-					image: true,
-					sectionId: true,
-					modules: {
-						select: {
-							name: true,
-							slug: true,
-							courseId: true,
-							lessons: {
-								select: {
-									title: true,
-									slug: true,
-									moduleId: true
+	try {
+		return await prismaClient.section.findFirst({
+			where: {
+				slug: sectionSlug
+			},
+			select: {
+				name: true,
+				slug: true,
+				courses: {
+					select: {
+						name: true,
+						slug: true,
+						image: true,
+						sectionId: true,
+						modules: {
+							select: {
+								name: true,
+								slug: true,
+								courseId: true,
+								lessons: {
+									select: {
+										title: true,
+										slug: true,
+										moduleId: true
+									}
 								}
 							}
 						}
+					},
+					where: {
+						slug: courseSlug
 					}
-				},
-				where: {
-					slug: courseSlug
 				}
 			}
-		}
-	});
+		});
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
 }
 
 export async function getLessonBySlug(
@@ -79,30 +109,35 @@ export async function getLessonBySlug(
 	moduleSlug: string,
 	lessonSlug: string
 ): Promise<sectionType | null> {
-	return await prismaClient.section.findFirst({
-		where: {
-			slug: sectionSlug
-		},
-		include: {
-			courses: {
-				where: {
-					slug: courseSlug
-				},
-				include: {
-					modules: {
-						where: {
-							slug: moduleSlug
-						},
-						include: {
-							lessons: {
-								where: {
-									slug: lessonSlug
+	try {
+		return await prismaClient.section.findFirst({
+			where: {
+				slug: sectionSlug
+			},
+			include: {
+				courses: {
+					where: {
+						slug: courseSlug
+					},
+					include: {
+						modules: {
+							where: {
+								slug: moduleSlug
+							},
+							include: {
+								lessons: {
+									where: {
+										slug: lessonSlug
+									}
 								}
 							}
 						}
 					}
 				}
 			}
-		}
-	});
+		});
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
 }
